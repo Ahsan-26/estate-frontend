9a7b0d9cd36069038453087f79075c5eb954f9cb
shayan5522 <shayanahmed743@gmail.com>
2025-02-18 21:26:20 +0500

    Cards Adjusted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ChakraProvider, Popover} from "@chakra-ui/react";
+import { ChakraProvider} from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
